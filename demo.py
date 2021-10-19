3376f7879d36908a89eb96c9d0cf56b76ba5563d
Phileco12 <62038140+Phileco12@users.noreply.github.com>
2021-10-19 21:24:06 +0200

    Update demo.py
    test

--- demo.py
+++ demo.py
@@ -154,4 +154,5 @@ if __name__ == "__main__":
     else:
         predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
     imageio.mimsave(opt.result_video, [img_as_ubyte(frame) for frame in predictions], fps=fps)
+    #test
 
